c5b9e7320fa1edd298dbbb581c6c312a9b98030c
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-16 22:31:37 -0300

    adicionado rota de testes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './pages/default/default.component';
+import { authGuard } from './services/auth/auth.guard';
+import { HeaderComponent } from './components/pageConfigs/header/header.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: DefaultComponent,
+        canActivate: [authGuard],
+        children: [
+            // home page
+            {
+                path: "",
+                component: DefaultComponent,
+                data: {
+                breadcrumb: {
+                    label: "",
+                },
+                },
+            },
+        ],
+
+        // login page
+        // {
+        //     path: "login",
+        //     component: HeaderComponent,
+        //     data: {
+        //     breadcrumb: {
+        //         label: "Login",
+        //     },
+        //     },
+        // },
+    }
+];
