c7f8f85c1a7b8798030aa675f77f37e2a86435fb
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-17 23:38:29 -0300

    alterado forma da rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,35 +2,10 @@ import { Routes } from '@angular/router';
 import { DefaultComponent } from './pages/default/default.component';
 import { authGuard } from './services/auth/auth.guard';
 import { HeaderComponent } from './components/pageConfigs/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: DefaultComponent,
-        canActivate: [authGuard],
-        children: [
-            // home page
-            {
-                path: "",
-                component: DefaultComponent,
-                data: {
-                breadcrumb: {
-                    label: "",
-                },
-                },
-            },
-        ],
-
-        // login page
-        // {
-        //     path: "login",
-        //     component: HeaderComponent,
-        //     data: {
-        //     breadcrumb: {
-        //         label: "Login",
-        //     },
-        //     },
-        // },
-    }
+    {path: "", component: HomeComponent, canActivate: [authGuard],},
+    {path: "home", component: DefaultComponent, canActivate: [authGuard],}
 ];
