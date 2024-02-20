fce573af2203bfbd8ff5aa9dea6141dbd3ddc64e
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-20 15:43:52 -0300

    feat: criado form para login. Criado componente da página. Adicionado rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { DefaultComponent } from './pages/default/default.component';
 import { authGuard } from './services/auth/auth.guard';
 import { HeaderComponent } from './components/pageConfigs/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent, canActivate: [authGuard],},
-    {path: "home", component: DefaultComponent, canActivate: [authGuard],}
+    {path: "", component: HomeComponent,},
+    {path: "home", component: DefaultComponent, canActivate: [authGuard],},
+    {path: "login", component: LoginComponent}
 ];
