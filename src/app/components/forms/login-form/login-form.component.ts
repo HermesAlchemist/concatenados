ae382585cf13320e9b873c07db7c7793f0cde9af
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-21 00:06:26 -0300

    style: feito tentativas de centralizacao de icones

--- src/app/components/forms/login-form/login-form.component.ts
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+  ],
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements AfterViewInit {
+  envelope = faEnvelope;
+  lock = faLock;
+  eye = faEye;
 
+  ngAfterViewInit(): void {
+    if (typeof window !== 'undefined') {
+      // focus on the 'email' input field
+      document.getElementById('email')!.focus();
+    }
+  }
+
+  
 }
