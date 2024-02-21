f10e5a669e309fe2ee26cb4f0fc707ae70d579c6
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-21 18:09:40 -0300

    adicionado rota de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { authGuard } from './services/auth/auth.guard';
 import { HeaderComponent } from './components/pageConfigs/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 
 export const routes: Routes = [
     {path: "", component: HomeComponent,},
     {path: "home", component: DefaultComponent, canActivate: [authGuard],},
-    {path: "login", component: LoginComponent}
+    {path: "login", component: LoginComponent},
+    {path: "register", component: CadastroComponent}
 ];
