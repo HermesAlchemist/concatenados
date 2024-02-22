982b58cf0d9d43edfd46bf4ce0a07c622082c5c8
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-22 19:48:07 -0300

    feat: componente para a home de user logado

---
+++ src/app/pages/logged-home/logged-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './logged-home.component.html',
+  styleUrl: './logged-home.component.scss'
+})
+export class LoggedHomeComponent {
+
+}
