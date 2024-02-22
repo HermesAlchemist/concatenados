407cdc8c8904a33babba7f9c4f605ad61a5972c3
Enéas Marques <eneasmarques@outlook.com.br>
2024-02-22 20:23:59 -0300

    Adiciona Ícones nos Inputs do Cadastro/Registro

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser, faAddressCard, faCalendarDays, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { HeaderComponent } from "../../components/pageConfigs/header/header.component";
 import { BodyComponent } from "../../components/pageConfigs/body/body.component";
 
@@ -7,8 +9,13 @@ import { BodyComponent } from "../../components/pageConfigs/body/body.component"
     standalone: true,
     templateUrl: './cadastro.component.html',
     styleUrl: './cadastro.component.scss',
-    imports: [HeaderComponent, BodyComponent]
+    imports: [HeaderComponent, BodyComponent, FontAwesomeModule]
 })
 export class CadastroComponent {
 
+    user = faUser;
+    adressCard = faAddressCard;
+    envelope = faEnvelope;
+    phone = faPhone;
+    calendar = faCalendarDays;
 }
