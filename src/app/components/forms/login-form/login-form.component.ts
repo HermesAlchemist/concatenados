1c61190d30cd4ff775dd4234809c86760626c21e
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2024-02-22 19:05:20 -0300

    refactor: adicionado mensagem de falha. Adicionado campo 'rememberMer'

--- src/app/components/forms/login-form/login-form.component.ts
+++ src/app/components/forms/login-form/login-form.component.ts
@@ -4,6 +4,8 @@ import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { log } from 'console';
 
 @Component({
   selector: 'app-login-form',
@@ -11,6 +13,8 @@ import { AuthService } from '../../../services/auth/auth.service';
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
   imports: [
+    CommonModule,
+
     FontAwesomeModule,
     ReactiveFormsModule,
   ],
@@ -22,10 +26,13 @@ export class LoginFormComponent implements AfterViewInit {
   lock = faLock;
   eye = faEye;
 
+  isFormValid: boolean | null = null;
+
   // login form
   form: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl(false),
   });
 
   ngAfterViewInit(): void {
@@ -35,12 +42,26 @@ export class LoginFormComponent implements AfterViewInit {
     }
   }
   
+  onSubmit() {
+    this.isValid();
+    if (this.isFormValid) {
+      // call the login function from the auth service
+      let response = this.authService.login(this.form.value);
+
+      if (!response) {
+        console.log("nao foi possivel logar");
+        return;
+      }
+      // redirect to home
+      this.authService.redirectToHome();
+    }
+  }
+
+  isValid(): void {
+    this.isFormValid = this.form.valid;
+  }
+
   redirectRecoverPassword() {
     this.authService.redirectToRecoverPassword();
-  }
-  
-  onSubmit() {
-    console.log('Logging in...');
-    console.log(this.form.valid);
   }
 }
